578d92995970f56804e3c896e029b79221961736
BaHbKAwrx <bahbkawrx@gmail.com>
2019-05-13 15:36:35 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Shmygovskii Ivan on 5/13/19.
+//  Copyright © 2019 Shmygovskii Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
